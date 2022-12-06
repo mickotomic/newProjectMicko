1fa75787254c8fc1997236454f65bb7d43be3c5c
mickotomic <milorad.tomic.87@gmail.com>
2022-12-06 20:50:05 +0100

    Update cas.js

--- cas.js
+++ cas.js
@@ -1,2 +1,23 @@
-// branimir changed this
-// micko changed this
+
+class Person {
+
+	constructor(name, email, phone) {
+	this.name = name;
+	this.email = email;
+	this.phone = phone;
+}
+	displayEmail() {
+		console.log(this.email);
+}
+
+    mergeName (){
+    return this.name + this.phone;
+}
+}
+
+const person = new Person("Branimir", "m@m.com", "065608999");
+person.displayEmail();
+
+console.log(person.name);
+console.log(person.mergeName());
+
